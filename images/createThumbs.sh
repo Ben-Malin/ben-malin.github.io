0f8fbed59368e07dd52c74e4f6e7114650495f03
Ben Malin <archer.malin@gmail.com>
2023-11-27 18:53:57 +1030

    thumbnail auto generation

--- images/createThumbs.sh
+++ images/createThumbs.sh
@@ -5,10 +5,14 @@
 
 for f in ./showcase/*; do
     if [[ $f == *.mp4 ]]; then
-        ffmpeg -i $f -vf "select=eq(n\,1)" -vframes 1 -q:v 2 ./showcaseThumbnails/$(basename $f .mp4).jpg
-        # make it 200x200
-        convert ./showcaseThumbnails/$(basename $f .mp4).jpg -resize 200x200 ./showcaseThumbnails/$(basename $f .mp4).jpg
+        ffmpeg -i $f -vf "select=eq(n\,1)" -vframes 1 -q:v 2 ./showcaseThumbnails/$(basename $f .mp4).png
+        convert ./showcaseThumbnails/$(basename $f .mp4).png -resize 200x200 ./showcaseThumbnails/$(basename $f .mp4).png
     else
         convert $f -resize 200x200 ./showcaseThumbnails/$(basename $f)
+        # if not a png, convert
+        if [[ $f != *.png ]]; then
+            convert ./showcaseThumbnails/$(basename $f) ./showcaseThumbnails/$(basename $f .jpg).png
+            rm ./showcaseThumbnails/$(basename $f)
+        fi
     fi
 done
